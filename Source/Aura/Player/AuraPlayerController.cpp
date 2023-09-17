efb02567dc0579bc9e5c80fe63b7f6d6aa3e2143
ratelbytes <achristianrose@gmail.com>
2023-09-17 21:08:02 +0300

    1-9. We create a Move callback for our Move Input Action and Bind it in the Player Controller class

--- Source/Aura/Player/AuraPlayerController.cpp
+++ Source/Aura/Player/AuraPlayerController.cpp
@@ -1,8 +1,10 @@
 // Copyright Ratelbytes
 
+#include "Player/AuraPlayerController.h"
 #include "InputMappingContext.h"
 #include "EnhancedInputSubsystems.h"
-#include "Player/AuraPlayerController.h"
+#include "EnhancedInputComponent.h"
+
 
 AAuraPlayerController::AAuraPlayerController()
 {
@@ -19,7 +21,7 @@ void AAuraPlayerController::BeginPlay()
 
 	Subsystem->AddMappingContext(AuraContext, 0);
 
-	/* Cursor Settings */
+	/* Mouse & Cursor Settings */
 	bShowMouseCursor = true;
 	DefaultMouseCursor = EMouseCursor::Default;
 	FInputModeGameAndUI InputModeData;
@@ -28,4 +30,43 @@ void AAuraPlayerController::BeginPlay()
 	SetInputMode(InputModeData);
 
 	
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FVector ForwardDirection = GetDirectionFromYaw(EAxis::X);
+	const FVector RightDirection = GetDirectionFromYaw(EAxis::Y);
+
+	// We use Lazy-loading for cached pawn
+	if(!CachedPawn.IsValid())
+	{
+		CachedPawn = GetPawn<APawn>();
+	}
+	
+	if (CachedPawn.IsValid())
+	{
+		CachedPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		CachedPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
+
+FVector AAuraPlayerController::GetDirectionFromYaw(EAxis::Type Axis)
+{
+	const FRotator ControllerRotation = GetControlRotation();
+
+	// We take just the Yaw rotation of controller's rotation
+	const FRotator YawRotation(0.f, ControllerRotation.Yaw, 0.f);
+
+	// we take directions in unit vectors parallel to the floor
+	return FRotationMatrix(YawRotation).GetUnitAxis(Axis);
 }
--- Source/Aura/Player/AuraPlayerController.h
+++ Source/Aura/Player/AuraPlayerController.h
@@ -7,6 +7,8 @@
 #include "AuraPlayerController.generated.h"
 
 class UInputMappingContext;
+struct FInputActionValue;
+class UInputAction;
 
 UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
@@ -18,8 +20,19 @@ public:
 	
 protected:	
 	virtual void BeginPlay() override;
-
+	virtual void SetupInputComponent() override;
+	
 private:
+	void Move(const FInputActionValue& InputActionValue);
+
+	/* Get Right or Forward Vectors, parallel to floor for the controller */
+	FVector GetDirectionFromYaw(EAxis::Type Axis);
+	
 	UPROPERTY(EditAnywhere, Category="Input")
 	TObjectPtr<UInputMappingContext> AuraContext;
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	TWeakObjectPtr<APawn> CachedPawn = nullptr;
 };

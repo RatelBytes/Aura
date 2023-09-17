5555f67f3e0ea0c3cdfd8450041512e6d5555002
ratelbytes <achristianrose@gmail.com>
2023-09-17 19:54:36 +0300

    1-8. We setup Input in the Player Controller Class

--- Source/Aura/Player/AuraPlayerController.cpp
+++ Source/Aura/Player/AuraPlayerController.cpp
@@ -1,5 +1,31 @@
 // Copyright Ratelbytes
 
-
+#include "InputMappingContext.h"
+#include "EnhancedInputSubsystems.h"
 #include "Player/AuraPlayerController.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	checkf(IsValid(AuraContext), TEXT("AAuraPlayerController::BeginPlay InputMappingContext is not set"));
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	checkf(IsValid(Subsystem), TEXT("AAuraPlayerController::BeginPlay. UEnhancedInputLocalPlayerSubsystem* Subsystem is not set"))
+
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	/* Cursor Settings */
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+
+	
+}

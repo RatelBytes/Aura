ba724eb2f663fb81711b3b21e93da865c3cdda4a
ratelbytes <christian@ratelbytes.com>
2023-10-27 11:04:13 +0300

    94. Widget Attribute Tags

--- Source/Aura/UI/WidgetController/AuraAtributeMenuWidgetController.cpp
+++ Source/Aura/UI/WidgetController/AuraAtributeMenuWidgetController.cpp
@@ -17,7 +17,7 @@ void UAuraAtributeMenuWidgetController::BroadcastInitialValues()
 	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
 
 	check(AttributeInfo);
-
+	
 	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(FAuraGameplayTags::Get().Attributes_Primary_Strength);
 	Info.AttributeValue = AS->GetStrength();
 	AttributeInfoDelegate.Broadcast(Info);

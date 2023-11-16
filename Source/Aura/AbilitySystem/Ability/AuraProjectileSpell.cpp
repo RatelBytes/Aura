8be6941b1d98e251d3c5822c7d99c65b2d827d07
ratelbytes <christian@ratelbytes.com>
2023-11-16 16:25:40 +0200

    150. Custom Gameplay Effect Context

--- Source/Aura/AbilitySystem/Ability/AuraProjectileSpell.cpp
+++ Source/Aura/AbilitySystem/Ability/AuraProjectileSpell.cpp
@@ -47,8 +47,21 @@ void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocati
 		// Here we give the Projectile a Gameplay Effect Spec for causing Damage
 
 		const UAbilitySystemComponent* SourceASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetAvatarActorFromActorInfo());
-		const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), SourceASC->MakeEffectContext());
 
+		// We're adding lots of extra context
+		FGameplayEffectContextHandle EffectContextHandle = SourceASC->MakeEffectContext();
+		EffectContextHandle.SetAbility(this);
+		EffectContextHandle.AddSourceObject(Projectile);
+		TArray<TWeakObjectPtr<AActor>> Actors;
+		Actors.Add(Projectile);
+		EffectContextHandle.AddActors(Actors);
+		FHitResult HitResult;
+		HitResult.Location = ProjectileTargetLocation;
+		EffectContextHandle.AddHitResult(HitResult);
+
+		
+		const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), SourceASC->MakeEffectContext());
+		
 		const FAuraGameplayTags GameplayTags = FAuraGameplayTags::Get();
 		
 		const float ScaledDamage = Damage.GetValueAtLevel(10);
